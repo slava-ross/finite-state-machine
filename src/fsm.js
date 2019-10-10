3b035cfa93456171c4064859db7dabad105f2927
Yaroslav Yagodarov <slava-ross@yandex.ru>
2019-10-11 03:08:33 +0400

    feat: add task

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,61 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor( config ) {
+        this.currState = config.initial;
+        this.states = config.states;
+        this.histArray = [ this.currState ];
+        this.histPointer = 0;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.currState;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState( state ) {
+        if ( state in this.states ) {
+            this.currState = state;
+            this.histArray.push( state );
+            this.histPointer = this.histArray.length - 1;
+        } else {
+            throw new Error();
+        }
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger( event ) {
+        if ( event in this.states[ this.currState ].transitions ) {
+            this.currState = this.states[ this.currState ].transitions[ event ];
+            if ( this.histPointer === this.histArray.length - 1 ) {
+                this.histArray.push( this.currState );
+                this.histPointer++;
+            } else {
+                this.histArray.splice( this.histPointer + 1, this.histArray.length );
+                this.histArray.push( this.currState );
+                this.histPointer = this.histArray.length - 1;
+            }
+        } else {
+            throw new Error();
+        }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.currState = 'normal';
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +65,55 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates( event ) {
+        let stateArr = [];
+
+        for ( let state in this.states ) {
+            if ( event in this.states[ state ].transitions || typeof event === 'undefined' ) {
+                stateArr.push( state );
+            }
+        }
+        return stateArr;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if ( this.histPointer === 0 ) {
+            return false;
+        } else {
+            this.histPointer--;
+            this.currState = this.histArray[ this.histPointer ];
+            return true;            
+        }
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if ( this.histPointer === this.histArray.length - 1 ) {
+            return false;
+        } else {
+            this.histPointer++;
+            this.currState = this.histArray[ this.histPointer ];
+            return true;
+        }
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.histArray.splice( 0,this.histArray.length );
+        this.histArray = [ this.currState ];
+        this.histPointer = 0;
+    }
 }
 
 module.exports = FSM;
